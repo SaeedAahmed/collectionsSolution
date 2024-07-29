7771ce762ef47357c82f10b36a39bcecfeb009a6
SaeedAahmed <ss7377991@gmail.com>
2024-07-29 14:58:21 +0300

    Add project files.

---
+++ collections/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
