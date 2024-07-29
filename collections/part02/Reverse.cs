8d1cc04dd321c35eab6d0846a6032ff3ea1900d8
SaeedAahmed <ss7377991@gmail.com>
2024-07-29 21:07:29 +0300

    Solve all

---
+++ collections/part02/Reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace collections.reverse
+{
+     static class Reverse
+    {
+        public static void ReversaArrayList(ArrayList list)
+        {
+            
+            for (int i = 0; i < list.Count/2; i++)
+            {
+                var temp = list[i];
+                list[i] = list[list.Count-i-1];
+                list[list.Count - i - 1] = temp;
+            }
+        }
+    }
+}
